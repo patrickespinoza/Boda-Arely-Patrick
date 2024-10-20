e2db47e82f010c6ddada1de5e807d37b7a6328b2
patrick espinoza <patrickespinoza38@gmail.com>
2024-10-20 15:51:39 -0600

    cambio de portda en sm

--- Nuestra-Boda/src/portada.jsx
+++ Nuestra-Boda/src/portada.jsx
@@ -1,11 +1,11 @@
 export default function Portada() {
   return (
     <div className="w-full">
-      <section className="relative w-full h-[calc(100vh-240px)] md:h-screen flex flex-col items-center justify-center text-white">
+      <section className="relative w-full h-[calc(100vh-60px)] md:h-screen flex flex-col items-center justify-center text-white">
         <img
           src="/IMG_3539.JPG"
           alt="Fondo"
-          className="absolute inset-0 w-full h-full sm:object-cover"
+          className="absolute inset-0 w-full h-full object-cover"
         />
 
         <div className="relative bg-opacity-50 rounded-lg w-full h-full justify-center items-center flex flex-col p-4 gap-4">
