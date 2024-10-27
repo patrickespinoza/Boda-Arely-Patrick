d939f3429ae79fdb624268d1a202b5a7dbc97add
patrick espinoza <patrickespinoza38@gmail.com>
2024-10-27 14:57:54 -0600

    cambio de imagnes de fondo

--- Nuestra-Boda/src/portada.jsx
+++ Nuestra-Boda/src/portada.jsx
@@ -1,7 +1,9 @@
+
+
 export default function Portada() {
   return (
     <div className="w-full">
-      <section className="relative w-full h-[calc(100vh-160px)] md:h-screen flex flex-col items-center justify-center text-white">
+      <section className="relative w-full h-[calc(110vh-160px)] md:h-screen flex flex-col items-center justify-center text-white">
         <img
           src="/IMG_3539.JPG"
           alt="Fondo"
@@ -9,12 +11,12 @@ export default function Portada() {
         />
 
         <div className="relative bg-opacity-50 rounded-lg w-full h-full justify-center items-center flex flex-col p-4 gap-4">
-          <h1 className="p-4 text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-bold">
+          <h1 className="roboto-black p-4 text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-serif">
             Nuestra Boda
           </h1>
-          <p className="text-lg sm:text-2xl md:text-3xl lg:text-4xl">ARELY</p>
-          <p className="text-lg sm:text-2xl md:text-3xl lg:text-4xl">&</p>
-          <p className="text-lg sm:text-2xl md:text-3xl lg:text-4xl">PATRICK</p>
+          <p className="font-cursiveDancing text-lg sm:text-2xl md:text-3xl lg:text-4xl" >ARELY</p>
+          <p className="font-cursiveDancing text-lg sm:text-2xl md:text-3xl lg:text-4xl">&</p>
+          <p className="font-cursiveDancing text-lg sm:text-2xl md:text-3xl lg:text-4xl">PATRICK</p>
         </div>
       </section>
     </div>
--- Nuestra-Boda/tailwind.config.js
+++ Nuestra-Boda/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        roboto: ["Roboto", "sans-serif"],
+        cursive: ["Cedarville Cursive", "cursive"],
+        cursiveDancing: ["Dancing Script", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
