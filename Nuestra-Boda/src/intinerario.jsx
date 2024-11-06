492f09524095bfa43ac90daa48712a1011e89525
patrick espinoza <patrickespinoza38@gmail.com>
2024-11-05 21:36:44 -0600

    cambio de tamaño de letra

--- Nuestra-Boda/src/intinerario.jsx
+++ Nuestra-Boda/src/intinerario.jsx
@@ -76,17 +76,17 @@ export default function Intinerario() {
       <div className="flex-grow">
         <p className="text-left text-[16px] lg:text-[19px] font-bold">Fotografias</p>
         <img src="./camara-fotografica.png" alt="fotografia" className="h-20 w-20 mx-1 md:h-28 sm:w-28" />
-        <p className="text-left text-[13px] lg:text-[17px]">4:00 pm - 4:30 pm</p>
+        <p className="text-left text-[12px] lg:text-[17px]">4:00 pm - 4:30 pm</p>
       </div>
       <div className="flex-grow">
         <p className="text-right text-[16px] lg:text-[19px] font-bold">Comida</p>
         <img src="./cena-romantica.png" alt="comida" className="h-20 w-20 ml-auto md:h-28 sm:w-28" />
-        <p className="text-right text-[13px] lg:text-[17px]">5:00 pm - 6:00 pm</p>
+        <p className="text-right text-[12px] lg:text-[17px]">5:00 pm - 6:00 pm</p>
       </div>
       <div className="flex-grow">
         <p className="text-left text-[16px] lg:text-[19px] font-bold">Baile</p>
         <img src="./bola-de-disco.png" alt="baile" className="h-20 w-20  mx-0 md:h-28 sm:w-28" />
-        <p className="text-left text-[13px] lg:text-[17px]">6:30 pm - 9:30 pm</p>
+        <p className="text-left text-[12px] lg:text-[17px]">6:30 pm - 9:30 pm</p>
       </div>
     </div>
 
@@ -98,14 +98,14 @@ export default function Intinerario() {
     {/* Columna 3 */}
     <div className="flex flex-col gap-3 py-12">
       <div className="flex-grow">
-        <p className="text-right text-[15px] lg:text-[19px] font-bold">Llegada de Novios</p>
+        <p className="text-right text-[14px] lg:text-[19px] font-bold">Llegada de Novios</p>
         <img src="./arco-de-la-boda.png" alt="arco" className="h-20 w-20  ml-16 md:h-28 sm:w-28" />
-        <p className="text-right text-[13px] lg:text-[17px]">4:30 pm - 5:00 pm</p>
+        <p className="text-right text-[12px] lg:text-[17px]">4:30 pm - 5:00 pm</p>
       </div>
       <div className="flex-grow">
         <p className="text-left text-[16px] lg:text-[19px] font-bold">Pastel</p>
         <img src="./pastel.png" alt="pastel" className="h-20 w-20  md:h-28 sm:w-28" />
-        <p className="text-left text-[13px] lg:text-[17px]">6:00 pm - 6:30 pm</p>
+        <p className="text-left text-[12px] lg:text-[17px]">6:00 pm - 6:30 pm</p>
       </div>
       <div className="flex-grow">
         
