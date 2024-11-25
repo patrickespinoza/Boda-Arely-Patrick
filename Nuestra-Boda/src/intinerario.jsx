9332b1ad8b4037880c26f15f03779e4f6bf1b7b5
patrick espinoza <patrickespinoza38@gmail.com>
2024-11-24 19:27:26 -0600

    confirmacion de asistencia

--- Nuestra-Boda/src/intinerario.jsx
+++ Nuestra-Boda/src/intinerario.jsx
@@ -13,7 +13,9 @@ export default function Intinerario() {
     const data = { name, numPeople };
 
     try {
-      const response = await fetch("https://boda-backent-arely-patrick.onrender.com/confirmar-asistencia", {
+      // const response = await fetch("https://boda-backent-arely-patrick.onrender.com/confirmar-asistencia", {
+
+      const response = await fetch("http://localhost:5000/confirmar-asistencia", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -115,40 +117,22 @@ export default function Intinerario() {
   </div>
 </div>
 
-        {/* Sección de Confirmación de Asistencia */}
-        <div className="flex flex-col items-center justify-center gap-3 h-96 md:h-80 lg:h-[700px]" style={{ backgroundImage: 'url("/aa.JPG")', backgroundSize:'contain'}}>
-          <h1 className="text-xl sm:text-2xl font-bold p-5 font-playfair">CONFIRMAR ASISTENCIA</h1>
-          <img className="h-24 w-24 sm:h-28 sm:w-28 p-3" src="/anillos-de-boda.png" alt="dress code" />
-          <p className="text-lg sm:text-xl p-7 text-center font-cursiveDancing">Confirma tu asistencia antes de 11 de diciembre</p>
-          <button className="bg-yellow-600 rounded-md p-3 w-32 h-11 flex items-center justify-center text-white font-cursiveDancing" onClick={() => setIsOpen(true)}>Confirmar Asistencia</button>
-        </div>
-      </div>
+{/* Sección de Confirmación de Asistencia */}
+<div className="flex flex-col items-center justify-center gap-3 h-96 md:h-80 lg:h-[700px]" style={{ backgroundImage: 'url("/aa.JPG")', backgroundSize:'contain'}}>
+  <h1 className="text-xl sm:text-2xl font-bold p-5 font-playfair">CONFIRMAR ASISTENCIA</h1>
+  <img className="h-24 w-24 sm:h-28 sm:w-28 p-3" src="/anillos-de-boda.png" alt="dress code" />
+  <p className="text-lg sm:text-xl p-7 text-center font-cursiveDancing">Confirma tu asistencia antes de 11 de diciembre</p>
 
-      {/* Formulario de Confirmación */}
-      {isOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-5 rounded">
-            <h2 className="text-lg font-bold">Nombre</h2>
-            <input
-              type="text"
-              value={name}
-              placeholder="Escribe solo el nombre de un integrate"
-              onChange={(e) => setName(e.target.value)}
-              className="border p-2 rounded w-full"
-            />
-            <h2 className="text-lg font-bold">¿Cuántas personas asistirán?</h2>
-            <input
-              type="number"
-              value={numPeople}
-              min="1"
-              onChange={(e) => setNumPeople(e.target.value)}
-              className="border p-2 rounded w-full"
-            />
-            <button className="bg-blue-500 rounded-md p-2 mt-3 w-full text-white" onClick={handleConfirm}>Enviar Confirmación</button>
-            <button className="bg-red-600 rounded-md p-2 mt-3 w-full text-white" onClick={() => setIsOpen(false)}>Cerrar</button>
-          </div>
-        </div>
-      )}
+  {/* Botón para redirigir al Google Form */}
+  <button 
+    className="bg-yellow-600 rounded-md p-3 w-32 h-11 flex items-center justify-center text-white font-cursiveDancing"
+    onClick={() => window.location.href = "https://forms.gle/dpCus7rDMmoYdrmz9"}
+  >
+    Confirmar Asistencia
+  </button>
+</div>
+
+      </div>      
     </div>
   );
 }
