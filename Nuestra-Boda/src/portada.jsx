b28fb07d0b93ed7d2f9448fe217b8b2040ca5ff1
patrick espinoza <patrickespinoza38@gmail.com>
2024-11-25 19:00:19 -0600

    cambio de portada prueba 3

--- Nuestra-Boda/src/portada.jsx
+++ Nuestra-Boda/src/portada.jsx
@@ -28,7 +28,7 @@ export default function Portada() {
       </audio>
       <section className="relative w-full h-[calc(110vh-160px)] md:h-screen flex flex-col items-center justify-center text-white">
         <img
-          src="/puertafoto.jpeg"
+          src="/cargadafoto.jpeg"
           alt="Fondo"
           className="absolute w-full h-full object-cover"
         />
