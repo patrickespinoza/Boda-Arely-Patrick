4682e6d8388ea7cdd5142fbc827af830e6a8e4d6
patrick espinoza <patrickespinoza38@gmail.com>
2024-11-25 19:11:35 -0600

    cambio de portada oficial 1

--- Nuestra-Boda/src/portada.jsx
+++ Nuestra-Boda/src/portada.jsx
@@ -28,12 +28,12 @@ export default function Portada() {
       </audio>
       <section className="relative w-full h-[calc(110vh-160px)] md:h-screen flex flex-col items-center justify-center text-white">
         <img
-          src="/floresfoto.jpeg"
+          src="/puertafoto.jpeg"
           alt="Fondo"
           className="absolute w-full h-full object-cover"
         />
 
-        <div className="relative bg-opacity-50 rounded-lg w-full h-full justify-start items-center flex flex-col p-4 gap-4">
+        <div className="relative bg-opacity-50 rounded-lg w-full h-full justify-center items-center flex flex-col p-4 gap-4">
           <h1 className="roboto-black p-4 text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-serif">
             Nuestra Boda
           </h1>
