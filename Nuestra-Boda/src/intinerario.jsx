1c98d1cf9fc9b59c6f45dd56c0055ec35850a392
patrick espinoza <patrickespinoza38@gmail.com>
2024-11-25 20:06:54 -0600

    coreccion de escritura

--- Nuestra-Boda/src/intinerario.jsx
+++ Nuestra-Boda/src/intinerario.jsx
@@ -20,11 +20,11 @@ export default function Intinerario() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2  ">
         <div className="flex flex-col items-center justify-center gap-3 h-96 md:h-96 lg:h-[700px]" >
-          <h1 className="text-2xl sm:text-3xl font-bold p-5 font-playfair">CELEBRACION</h1>
+          <h1 className="text-2xl sm:text-3xl font-bold p-5 font-playfair">CELEBRACIÓN</h1>
           <p className="text-xl sm:text-xl font-cursiveDancing">11 | Enero | 2025</p>
           <p className="text-xl sm:text-xl font-cursiveDancing"> 4:30 PM</p>
           {/* <img className="h-24 w-24 sm:h-28 sm:w-28 p-3" src="/tarta-de-boda.png" alt="imagen del salon" /> */}
-          <p className="p-3 text-center font-cursiveDancing">Salon Maria Bonita Event Center</p>
+          <p className="p-3 text-center font-cursiveDancing">Salón María Bonita Event Center</p>
           <p className="p-3 text-center font-cursiveDancing">C.2 Pte. 905, Libertad, 72130 Heroica Puebla de Zaragoza, Pue</p>
           <a className="bg-yellow-600 rounded-md p-3 w-36 h-11 flex items-center justify-center text-xl text-white font-cursiveDancing" href="https://www.google.com/maps/place/Eventos+Mar%C3%ADa+Bonita+Puebla/@19.0703659,-98.24125,17z/data=!3m1!4b1!4m6!3m5!1s0x85cfc79834a1cd75:0xb0d61abeb78ef0fe!8m2!3d19.0703608!4d-98.2386751!16s%2Fg%2F11mtddfrw5?entry=ttu">Ver Dirección</a>
         </div>
@@ -49,7 +49,7 @@ export default function Intinerario() {
     {/* Columna 1 */}
     <div className="flex flex-col gap-3">
       <div className="flex-grow">
-        <p className="text-left text-[16px] lg:text-[19px] font-bold ml-2">Fotografias</p>
+        <p className="text-left text-[16px] lg:text-[19px] font-bold ml-2">Fotografías</p>
         <img src="./camara-fotografica.png" alt="fotografia" className="ml-2 h-20 w-20 mx-1 md:h-28 sm:w-28" />
         <p className="text-left text-[11px] lg:text-[17px] ml-2">4:00 pm - 4:30 pm</p>
       </div>
@@ -93,7 +93,7 @@ export default function Intinerario() {
 <div className="flex flex-col items-center justify-center gap-3 h-96 md:h-80 lg:h-[700px]" style={{ backgroundImage: 'url("/aa.JPG")', backgroundSize:'contain'}}>
   <h1 className="text-xl sm:text-2xl font-bold p-5 font-playfair">CONFIRMAR ASISTENCIA</h1>
   <img className="h-24 w-24 sm:h-28 sm:w-28 p-3" src="/anillos-de-boda.png" alt="dress code" />
-  <p className="text-lg sm:text-xl p-7 text-center font-cursiveDancing">Confirma tu asistencia antes de 11 de diciembre</p>
+  <p className="text-lg sm:text-xl p-7 text-center font-cursiveDancing">Confirma tu asistencia antes de 20 de diciembre</p>
 
   {/* Botón para redirigir al Google Form */}
   <button 
